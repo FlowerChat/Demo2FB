292a209cb0a25e30ce4e26cefae9568f8be1b2d6
FlowerChat <flowerchatbot@gmail.com>
2017-05-04 22:56:49 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
+
 import urllib
 import json
 import os
 
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import requests
+from key import key
+import imghdr
 from flask import request
 from flask import make_response
+#import psycopg2
+import datetime
+
+
+
+
+
+
+#str = unicode(str, errors='ignore')
+
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -19,146 +33,53 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = makeWebhookResult(req)
 
     res = json.dumps(res, indent=4)
-    # print(res)
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
+def makeWebhookResult(req):
+    
+    if req.get("result").get("action") == "input.welcome":
+        
+    
+        TimeStamp=str(datetime.datetime.utcnow())
 
-def processRequest(req):
-    if req.get("result").get("action") != "input.welcome":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    print(yql_url)
+     
+    
 
-    result = urllib.urlopen(yql_url).read()
-    print("yql result: ")
-    print(result)
+    
 
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
+        speech = TimeStamp
 
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = "Paris"
-    if city is None:
-        return None
+        print("Response:")
+        print(speech)
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    print(speech)
-
-    slack_message = {
-        "text": speech,
-        "attachments": [
-            {
-                "title": channel.get('title'),
-                "title_link": channel.get('link'),
-                "color": "#36a64f",
-
-                "fields": [
-                    {
-                        "title": "Condition",
-                        "value": "Temp " + condition.get('temp') +
-                                 " " + units.get('temperature'),
-                        "short": "false"
-                    },
-                    {
-                        "title": "Wind",
-                        "value": "Speed: " + channel.get('wind').get('speed') +
-                                 ", direction: " + channel.get('wind').get('direction'),
-                        "short": "true"
-                    },
-                    {
-                        "title": "Atmosphere",
-                        "value": "Humidity " + channel.get('atmosphere').get('humidity') +
-                                 " pressure " + channel.get('atmosphere').get('pressure'),
-                        "short": "true"
-                    }
-                ],
-
-                "thumb_url": "http://l.yimg.com/a/i/us/we/52/" + condition.get('code') + ".gif"
+        facebook_message = {
+            "text": TimeStamp
             }
-        ]
-    }
 
-    facebook_message = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [
-                    {
-                        "title": channel.get('title'),
-                        "image_url": "http://l.yimg.com/a/i/us/we/52/" + condition.get('code') + ".gif",
-                        "subtitle": speech,
-                        "buttons": [
-                            {
-                                "type": "web_url",
-                                "url": channel.get('link'),
-                                "title": "View Details"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
-
-    print(json.dumps(slack_message))
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        "data": {"slack": slack_message, "facebook": facebook_message},
+        print(json.dumps(facebook_message))
+        return {
+            "speech": speech,
+            "displayText": speech,
+            "data": {"facebook": facebook_message},
         # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
+            "contextOut": [{"name":"flowerchatline", "lifespan":5},{"name":"choose-florist", "lifespan":1}]
+        }
+    return {}
+    
+
+    
+
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print "Starting app on port %d" % port
-
-app.run(debug=False, port=port, host='0.0.0.0')
+app.run(debug=True, port=port, host='0.0.0.0')
