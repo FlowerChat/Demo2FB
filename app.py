a6269142c8b84c1114a094f1be0b395d55d74078
FlowerChat <flowerchatbot@gmail.com>
2017-05-04 23:37:24 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def makeWebhookResult(req):
         facebook_message = {
                 "text": TimeStamp
             },
-                {
+            {
                 "buttons":[
                     {
                         "type":"phone_number",
