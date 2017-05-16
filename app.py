60dc991b0c6cd04175a07821affebecbebacbed8
FlowerChat <flowerchatbot@gmail.com>
2017-05-16 17:15:13 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ import datetime
 
 
 
-
-#str = unicode(str, errors='ignore')
-
-
 # Flask app should start in global layout
 app = Flask(__name__)
 
@@ -43,25 +39,52 @@ def webhook():
 
 def makeWebhookResult(req):
     
-    if req.get("result").get("action") == "input.welcome":
+    if req.get("result").get("action") == "firstfb":
+        #result= req.get("result")
+        #contexts=result.get("contexts")
+        #generic_con=contexts{"name":"generic"}
+
+        #generic_conparams=generic_con.get("parameters")
+    
+        #facebook_id=str(generic_conparams.get("facebook_sender_id"))
+        #user_id_url="https://graph.facebook.com/v2.6/"+facebook_id+"?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAARq6hqpYzMBACdg4Y2PXnoc8YlDkKysqZClfKC0X09aZBvklWsoNZAMP00ZCvrnm0O6nT2n1gh7YhDCnYvGWVbpPtzK5ZAa6qsjm98ZCZCmnmbc0hDZBBz6WGCSBCQ3Vm4FYnZBkyJdkbdHjZCHh98VOn8tM64Lyqvik3o2l23OfGIgZDZD"
+        #user_req=requests.get(user_id_url)
+        #user_json=user_req.json()
+        #facebook_user_firstname=user_json["first_name"]
+        
+        TimeStamp=str(datetime.datetime.utcnow())
+        
+
+        speech = TimeStamp
+        
+        print("Response:")
+        print speech
+        facebook_message = {
+            "text": "Hi, "+ TimeStamp +", I am a FlowerChat bot who will help you find the best florist"
+        }
+
+        
+        print(json.dumps(facebook_message))
+        return {
+            "data":{"facebook":facebook_message},
+            "contextOut": [{"name":"facebook_location", "lifespan":5},{"name":"flowerchatline","facebook_user_first":facebook_user_firstname,"lifespan":100}]
+        }
+    
+        
+        
+            
+    elif req.get("result").get("action") == "input.welcome":
         
     
         TimeStamp=str(datetime.datetime.utcnow())
-        #result = req.get("result")
-        #contexts=result.get("contexts")
         
-        
-
-     
-    
-
-    
 
         speech = TimeStamp
 
 
         print("Response:")
         print(speech)
+        
 
         facebook_message = {
             "text":"For better service please share your current location:",
@@ -91,7 +114,8 @@ def makeWebhookResult(req):
     
         CustLong=str(conparams.get("long"))
         CustLat=str(conparams.get("lat"))
-        generic_con=contexts[3]
+        #generic_con=contexts[3]
+        generic_con=contexts{"name":"generic"}
 
         generic_conparams=generic_con.get("parameters")
     
