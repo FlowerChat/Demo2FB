a9d990b08d723c4bf9c046fabea974e5cfcafafe
FlowerChat <flowerchatbot@gmail.com>
2017-05-17 13:22:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ def makeWebhookResult(req):
         print(json.dumps(facebook_message))
         return {
             "data":{"facebook":facebook_message},
-            "contextOut": [{"name":"facebook_location", "lifespan":0},{"name":"flowerchatline","facebook_user_first":facebook_user_firstname,"lifespan":100}]
+            "contextOut": [{"name":"facebook_location", "lifespan":0},{"name":"generic","lifespan":0},{"name":"flowerchatline","facebook_user_first":facebook_user_firstname,"lifespan":100}]
         }
 
         
