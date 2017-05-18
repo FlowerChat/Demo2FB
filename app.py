5584a61c39417ee268c80c88f04b993f934b7fb5
FlowerChat <flowerchatbot@gmail.com>
2017-05-18 18:28:59 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -66,36 +66,35 @@ def makeWebhookResult(req):
                     "template_type":"generic",
                     "elements":[
                         {
-                            "title":"Hi, "+ facebook_user_firstname +", I can help you find the best local florist",                            
-                            
-                            "image_url":"http://www.fiorita.cz/wp-content/uploads/2017/03/kvetinarstvi-praha-jarni-kytice-tulipany-anemony-pryskyrniky.jpg",
-                            "subtitle":"This bouquet is tied by my developers. You can order flowers through their web",
-                            "default_action": {
-                                "type": "web_url",
-                                "url": "http://www.fiorita.cz/",
-                                "messenger_extensions": "true",
-                                "webview_height_ratio": "tall",
-                                "fallback_url": "http://www.fiorita.cz/"
-                            },
-                            "buttons":[
-                                {
-                                    "type":"web_url",
-                                    "url":"http://www.fiorita.cz",
-                                    "title":"View Website"
-                                },{
-                                    "type":"postback",
-                                    "title":"Start Chatting",
-                                    "payload":"Yes, please"
-                                },{
-                                    "type":"postback",
-                                    "title":"Quit",
-                                    "payload":"No, thanks"
-                                }              
-                            ]      
-                        }
-                    ]
-                }
-            }
+                          "title":"Hi, "+ facebook_user_firstname +", I can help you find the best local florist",                            
+                          "image_url":"http://www.fiorita.cz/wp-content/uploads/2017/03/kvetinarstvi-praha-jarni-kytice-tulipany-anemony-pryskyrniky.jpg",
+                          "subtitle":"This bouquet was tied by my developers. You can order flowers through their web",
+                          "default_action": {
+                              "type": "web_url",
+                              "url": "http://www.fiorita.cz/",
+                              "messenger_extensions": "true",
+                              "webview_height_ratio": "tall",
+                              "fallback_url": "http://www.fiorita.cz/"
+                          },
+                          "buttons":[
+                              {
+                                "type":"web_url",
+                                "url":"http://www.fiorita.cz",
+                                "title":"View Website"
+                              },{
+                                "type":"postback",
+                                "title":"Start Chatting",
+                                "payload":"Yes, please"
+                              },{
+                                "type":"postback",
+                                "title":"Quit",
+                                "payload":"No, thanks"
+                              }              
+                          ]      
+                      }
+                  ]
+              }
+          }
         }
 
         
