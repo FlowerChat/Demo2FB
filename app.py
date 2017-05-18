c288ec83f90f518ae91f6e336ed7d3ddacbc7ef9
FlowerChat <flowerchatbot@gmail.com>
2017-05-19 00:14:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def makeWebhookResult(req):
                         {
                             "title":"Hi, "+ facebook_user_firstname +", I can help you find the best local florist",                            
                             "image_url":"http://www.fiorita.cz/wp-content/uploads/2017/03/kvetinarstvi-praha-jarni-kytice-tulipany-anemony-pryskyrniky.jpg",
-                            "subtitle":"This bouquet was tied by my developers. You can order flowers through their web",
+                            "subtitle":"The bouquet was tied by my developers. You can order flowers through their web",
                             "default_action":{
                                 "type": "web_url",
                                 "url": "http://www.fiorita.cz/",
