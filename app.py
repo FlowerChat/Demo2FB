36f5f1c06f03372aeb31ffcd217a902aec6fcc62
FlowerChat <flowerchatbot@gmail.com>
2017-05-19 13:49:47 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def makeWebhookResult(req):
                             "title":"Hi, "+ facebook_user_firstname +", I can help you find the best local business"                            
                             #"image_url":
                             #}
-                        },{
+                        #},{
                             "buttons":[
                                 {
                                     "type":"web_url",
